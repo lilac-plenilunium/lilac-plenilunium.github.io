6492647a94c887fbaf751ea70f9cafd3eeaa968f
lilac-plenilunium <akizetesche@gmail.com>
2025-06-09 17:02:43 -0300

    ok fonts are actually fixed now

--- script.js
+++ script.js
@@ -236,3 +236,16 @@ if (document.getElementById("footer")) {
 if (document.title === "Blog Post") {
   document.title = currentPostTitle;
 }
+
+//
+let cursor = document.querySelector('.cursor')
+
+document.addEventListener('mousemove', moveCursor)
+
+function moveCursor(e) {
+  let x = e.clientX;
+  let y = e.clientY;
+
+  cursor.style.left = `${x}px`
+  cursor.style.top = `${y}px`
+}
